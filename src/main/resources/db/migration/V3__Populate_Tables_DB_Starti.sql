34ea0a3bc8a45e076b95b18bd2d1addc8ec90dc5
barbaradsp <barbaradspimenta@gmail.com>
2022-07-11 17:38:42 -0300

    Criação de arquivo para popular o banco de dados

---
+++ src/main/resources/db/migration/V3__Populate_Tables_DB_Starti.sql
@@ -0,0 +1,3 @@
+INSERT INTO endereco VALUES (3,'Jardins','33910-30','Belo Horizonte','','1940','Avenida das Andorinhas','MG'),(4,'Nova União','42681-715','São Paulo','','870','Rua das Pedrinhas','SP');
+INSERT INTO pessoa_fisica VALUES (1,'mariajose@google.com','(31)99876-5432','','000.000.000-00','1978-07-02 00:00:00',NULL,NULL,'Maria','José',3),(2,'antoniob@google.com','(11)99876-5432','','111.111.111-11','1982-03-22 00:00:00',NULL,NULL,'Antonio','Barcelos',4);
+INSERT INTO vaga VALUES (1,'Estágio','Estágio em Desenvolvimento Web na Neoway','Home Office','2022-09-03 00:00:00',1500,1),(3,'Dev Front-end Jr','Desenvolvedor front-end júnior. Não é necessaria experiência prévia.','X','2022-10-10 00:00:00',3400,2);
