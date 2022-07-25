dcef5f4b5a4e54935731234aec5ca30219836e9d
Dayane Leal <dayane_leal2004@hotmail.com>
2022-07-25 18:47:41 -0300

    Construção da páginas inicial

---
+++ Layout/assets/js/main.js
@@ -0,0 +1,56 @@
+;(function () {
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+  /**
+   * Preloader
+   */
+  let preloader = select('#preloader')
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove()
+    })
+  }
+
+  /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+  let selectHeader = select('#header')
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled')
+      } else {
+        selectHeader.classList.remove('header-scrolled')
+      }
+    }
+    window.addEventListener('load', headerScrolled)
+    onscroll(document, headerScrolled)
+  }
+})()
--- src/main/java/br/com/starti/config/SecurityConfig.java
+++ src/main/java/br/com/starti/config/SecurityConfig.java
@@ -55,6 +55,6 @@ public class SecurityConfig {
 	    }
 	 
 	 public static void main(String[] args) {
-		System.out.println(new BCryptPasswordEncoder().encode("123456"));
+		System.out.println(new BCryptPasswordEncoder().encode("admin1234"));
 	}
 }
