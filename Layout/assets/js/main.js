08d8c1a9365163ef8ce72b368f50def6b269b1b6
Dayane Leal <dayane_leal2004@hotmail.com>
2022-07-25 22:02:04 -0300

    Continuação da página inicial

--- Layout/assets/js/main.js
+++ Layout/assets/js/main.js
@@ -53,4 +53,30 @@
     window.addEventListener('load', headerScrolled)
     onscroll(document, headerScrolled)
   }
+
+  new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      }
+    }
+  })
 })()
